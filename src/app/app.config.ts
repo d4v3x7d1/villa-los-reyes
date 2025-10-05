e6cc1446645dba4caa6f0faa3f3e2c4b49d941cc
davidalba <david2001.004@gmail.com>
2025-10-05 04:10:47 -0400

    feat(app): actualizar estructura y layout principal

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,25 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+
+    provideHttpClient(),
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({
+        prefix: '/assets/i18n/',
+        suffix: '.json'
+      }),
+      fallbackLang: 'es',
+      lang: 'es'
+    })
   ]
 };
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Counter } from './garbage-components/counter';
+import { HomePage } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'home',
+    component:HomePage
+  }
+];
