bc8a90d4287f9e9a48d0223e19aa2cbee47b3817
davidalba <david2001.004@gmail.com>
2025-11-26 02:32:00 -0500

    Una locura

--- src/app/components/sections/discover-room-section/rooms-gallery/room-item/room-item.ts
+++ src/app/components/sections/discover-room-section/rooms-gallery/room-item/room-item.ts
@@ -1,15 +1,20 @@
 import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'room-item',
   templateUrl: './room-item.html',
-  styleUrl: './room-item.css',
-  imports:[TranslatePipe]
+  styleUrls: ['./room-item.css'],  // corregido: era styleUrl → styleUrls
+  imports: [TranslatePipe,RouterLink]
 })
 export class RoomItem {
   src = input<string>('');
-  alt =input<string>('');
+  alt = input<string>('');
   name = input<string>('');
   details = input<string>('');
+  link = input<string>();
+
+  // Nuevo input para controlar el contexto/variante
+  variant = input<'default' | 'discover'>('default');
 }
--- src/app/components/sections/grid-rooms-section/grid-rooms-section.ts
+++ src/app/components/sections/grid-rooms-section/grid-rooms-section.ts
@@ -16,6 +16,6 @@ export class GridRoomsSection {
   }>();
 
   rooms = input<
-    { src: string; alt: string; name: string; details: string }[]
+    { src: string; alt: string; name: string; details: string; link?: string }[]
   >([]);
 }
